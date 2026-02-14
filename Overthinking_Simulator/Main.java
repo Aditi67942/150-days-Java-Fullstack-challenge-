90779b1e2c092680e01db2b1ec6e0ebfabb4413d
Aditi Jha <ajha67942@gmail.com>
2026-02-15 01:54:15 +0530

    Add Expansion instance to Main class
    Added an Expansion class instance to handle additional thoughts and their resolutions.

--- Overthinking_Simulator/Main.java
+++ Overthinking_Simulator/Main.java
@@ -1,9 +1,18 @@
 package Overthinking_Simulator;
 
 public class Main
+    //13/02/26 code
 {
         public static void main(String[] args) {
             Thought t = new Thought("What if I fail in this life?", "anxious", "exam", "i overthink for 2 days then somehow accept it");
             System.out.println(t);
-        }
+
+           //14/02/26 code
+           Expansion e = new Expansion(
+                   "I can't forget her",
+                   "went outside, walked around, felt better",
+                   "happens every 2 days, always passes"
+           );
+        System.out.println(e);
+}
 }
